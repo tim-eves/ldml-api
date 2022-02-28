dd9754d8c2c49cab977f7942f99f1f4a8ae8ee1f
Tim Eves <tim_eves@sil.org>
2022-02-28 14:40:49 +0700

    Add tests for langtags and fix bugs
    Gracefully skip blank lines.
    Error on tagset with missing = delimiters.
    Fix Display trait implementation for TagSet.

--- src/langtags.rs
+++ src/langtags.rs
@@ -9,12 +9,12 @@ use std::{
 use crate::tag::Tag;
 
 #[derive(Debug,Eq,PartialEq)]
-pub struct TagSet(pub HashSet<Tag>);
+pub struct TagSet(HashSet<Tag>);
 
 type LangTagsInner = HashMap<Tag, Arc<TagSet>>;
 
-#[derive(Debug)]
-pub struct LangTags(pub LangTagsInner);
+#[derive(Clone, Debug, PartialEq)]
+pub struct LangTags(LangTagsInner);
 
 
 impl LangTags {
@@ -23,15 +23,21 @@ impl LangTags {
         where E: Into<Box<dyn Error + Send + Sync>> {
             io::Error::new(io::ErrorKind::InvalidData, error)
         }
-
         let parse = |s: &str| s.trim().trim_start_matches('*').parse::<Tag>();
         let mut buf = String::new();
         reader.read_to_string(&mut buf)?;
 
         let mut map = LangTagsInner::new();
-        for parses in buf.lines().map(|l| l.split('=').map(parse)) {
+        for (i, parses) in buf.lines().filter(|l| !l.trim().is_empty()).map(|l| l.split('=').map(parse)).enumerate() {
             let tagset = Arc::new(TagSet(parses.collect::<Result<HashSet<Tag>, _>>()
                 .map_err(into_io_error)?));
+
+            if tagset.len() <= 1 {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    format!("line {}: missing, one or more, '=' in TagSet specification", i+1)));
+            }
+            
             map.extend(tagset.iter().cloned().map(|t| (t, tagset.clone())));
         }
         Ok(LangTags(map))
@@ -57,11 +63,81 @@ impl DerefMut for TagSet {
 impl Display for TagSet {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut tagset: Vec<_> = self.iter().collect();
-        tagset.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        let mut s = tagset.iter().fold(
-            String::new(), 
-            |s, ts| s + &ts.to_string() + "=");
-        s.pop();
+        tagset.sort();
+        let s = tagset.iter()
+            .map(|t| t.to_string())
+            .reduce(|accum, item| accum + " = " + &item)
+            .unwrap_or_default();
         f.write_str(&s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::HashSet,
+        io,
+        iter::FromIterator,
+        sync::Arc
+     };
+    use crate::tag::Tag;
+    use super::{ LangTags, LangTagsInner, TagSet };
+
+    #[test]
+    fn invalid_tag() {
+        let test = LangTags::from_reader(&b"#*aa = *aa-ET = aa-Latn = aa-Latn-ET"[..]).err().unwrap();
+        assert_eq!(test.kind(), io::ErrorKind::InvalidData);
+        assert_eq!(test.to_string(), "Parsing Error: (\"#*aa\", Tag)");
+    }
+
+
+    #[test]
+    fn invalid_tagset() {
+        let test = LangTags::from_reader(&b"aa aa-ET aa-Latn aa-Latn-ET"[..]).err().unwrap();
+        assert_eq!(test.to_string(), "line 1: missing, one or more, '=' in TagSet specification");
+    }
+
+    #[test]
+    fn load_minimal_langtags() {
+        let test = LangTags::from_reader(&br#"
+            *aa = *aa-ET = aa-Latn = aa-Latn-ET
+            aa-Arab = aa-Arab-ET"#[..]).ok().unwrap();
+        let aa: Arc<TagSet> = Arc::new(TagSet(HashSet::from_iter([
+            Tag::lang("aa"), 
+            Tag::lang("aa").region("ET"), 
+            Tag::lang("aa").script("Latn"),
+            Tag::lang("aa").script("Latn").region("ET")])));
+        let aa_arab: Arc<TagSet> = Arc::new(TagSet(HashSet::from_iter([
+            Tag::lang("aa").script("Arab"), 
+            Tag::lang("aa").script("Arab").region("ET")])));
+        let mut expected = LangTagsInner::new();
+        expected.extend(aa.iter().cloned().map(|t| (t, aa.clone())));
+        expected.extend(aa_arab.iter().cloned().map(|t| (t, aa_arab.clone())));
+
+        assert_eq!(test.0, expected);
+    }
+
+    #[test]
+    fn display_trait() {
+        let mut test: Vec<_> = LangTags::from_reader(&br#"
+            *aa = *aa-ET = aa-Latn = aa-Latn-ET
+            aa-Arab = aa-Arab-ET"#[..])
+            .ok()
+            .unwrap()
+            .iter()
+            .map(|(k, v)| format!("{k}: {v}"))
+            .collect();
+        test.sort();
+
+        assert_eq!(
+            test, 
+            [
+                "aa-Arab-ET: aa-Arab = aa-Arab-ET",
+                "aa-Arab: aa-Arab = aa-Arab-ET",
+                "aa-ET: aa = aa-ET = aa-Latn = aa-Latn-ET",
+                "aa-Latn-ET: aa = aa-ET = aa-Latn = aa-Latn-ET",
+                "aa-Latn: aa = aa-ET = aa-Latn = aa-Latn-ET",
+                "aa: aa = aa-ET = aa-Latn = aa-Latn-ET",
+            ]);
     }
 }
