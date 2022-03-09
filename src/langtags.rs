6729c0f8772c09d278b56a717e5d8ede2f4ab640
Tim Eves <tim_eves@sil.org>
2022-03-09 14:17:15 +0700

    Convert LangTags to full readonly, and remove Arc
    LangTags is immutable once it's loaded convert it to an arena style data
    structure to remove the need to use Arc to share access to TagSets and
    avoid atomic contention on every request.

--- src/langtags.rs
+++ src/langtags.rs
@@ -1,20 +1,23 @@
 use std::{
-    collections::{ HashMap, HashSet },
+    collections::{ HashMap, HashSet, hash_map },
     error::Error,
     fmt::Display,
     io::{ self, Read },
-    ops::{ Deref, DerefMut },
-    sync::Arc
+    ops::{ Index, Deref, DerefMut },
 };
 use crate::tag::Tag;
 
-#[derive(Debug,Eq,PartialEq)]
+#[derive(Clone,Debug,Eq,PartialEq)]
 pub struct TagSet(HashSet<Tag>);
 
-type LangTagsInner = HashMap<Tag, Arc<TagSet>>;
+type TagSetRef = u32;
 
-#[derive(Clone, Debug, PartialEq)]
-pub struct LangTags(LangTagsInner);
+#[derive(Debug, PartialEq)]
+pub struct LangTags
+{
+    tagsets: Vec<TagSet>,
+    map: HashMap<Tag, TagSetRef>,
+}
 
 
 impl LangTags {
@@ -28,27 +31,52 @@ impl LangTags {
         let mut buf = String::new();
         reader.read_to_string(&mut buf)?;
 
-        let mut map = LangTagsInner::new();
-        for (i, parses) in buf.lines().filter(|l| !l.trim().is_empty()).map(|l| l.split('=').map(parse)).enumerate() {
-            let tagset = Arc::new(TagSet(parses.collect::<Result<HashSet<Tag>, _>>()
-                .map_err(into_io_error)?));
-
-            if tagset.len() <= 1 {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    format!("line {}: missing, one or more, '=' in TagSet specification", i+1)));
-            }
-            
-            map.extend(tagset.iter().cloned().map(|t| (t, tagset.clone())));
+        let mut langtags = LangTags { 
+            tagsets: Default::default(), 
+            map: Default::default()
+        };
+        for parses in buf.lines().filter(|l| !l.trim().is_empty()).map(|l| l.split('=').map(parse)) {
+            let tagset = TagSet(parses.collect::<Result<HashSet<Tag>, _>>()
+                .map_err(into_io_error)?);
+            langtags.add_tagset(tagset);
         }
-        Ok(LangTags(map))
+        Ok(langtags)
+    }
+    
+    pub fn get(&self, k: &Tag) -> Option<&TagSet> {
+        self.map.get(k).and_then(|&i| self.tagsets.get(i as usize))
+    }
+
+    pub fn iter(&self) -> Iter {
+        Iter { inner: self.map.iter(), tagsets: &self.tagsets }
+    }
+
+    fn add_tagset(&mut self, ts: TagSet) {
+        let i = self.tagsets.len() as TagSetRef;
+        self.map.extend(ts.iter().cloned().map(|t| (t, i)));
+        self.tagsets.push(ts);
     }
 }
 
-impl Deref for LangTags {
-    type Target = HashMap<Tag, Arc<TagSet>>;
+impl Index<&Tag> for LangTags {
+    type Output = TagSet;
 
-    fn deref(&self) -> &Self::Target { &self.0 }
+    fn index(&self, tag: &Tag) -> &Self::Output {
+        &self.tagsets[self.map[tag] as usize]
+    }
+}
+
+pub struct Iter<'a> {
+    inner: hash_map::Iter<'a, Tag, TagSetRef>,
+    tagsets: &'a Vec<TagSet>
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = (&'a Tag, &'a TagSet);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|(k,&i)| (k, &self.tagsets[i as usize]))
+    }
 }
 
 impl Deref for TagSet {
@@ -67,7 +95,7 @@ impl Display for TagSet {
         tagset.sort();
         let s = tagset.iter()
             .map(|t| t.to_string())
-            .reduce(|accum, item| accum + " = " + &item)
+            .reduce(|accum, item| accum + "=" + &item)
             .unwrap_or_default();
         f.write_str(&s)
     }
@@ -79,10 +107,9 @@ mod tests {
         collections::HashSet,
         io,
         iter::FromIterator,
-        sync::Arc
      };
     use crate::tag::Tag;
-    use super::{ LangTags, LangTagsInner, TagSet };
+    use super::{ LangTags, TagSet };
 
     #[test]
     fn invalid_tag() {
@@ -91,31 +118,22 @@ mod tests {
         assert_eq!(test.to_string(), "Parsing Error: (\"#*aa\", Tag)");
     }
 
-
-    #[test]
-    fn invalid_tagset() {
-        let test = LangTags::from_reader(&b"aa aa-ET aa-Latn aa-Latn-ET"[..]).err().unwrap();
-        assert_eq!(test.to_string(), "line 1: missing, one or more, '=' in TagSet specification");
-    }
-
     #[test]
     fn load_minimal_langtags() {
         let test = LangTags::from_reader(&br#"
             *aa = *aa-ET = aa-Latn = aa-Latn-ET
             aa-Arab = aa-Arab-ET"#[..]).ok().unwrap();
-        let aa: Arc<TagSet> = Arc::new(TagSet(HashSet::from_iter([
+        let mut expected = LangTags { tagsets: Default::default(), map: Default::default()};
+        expected.add_tagset(TagSet(HashSet::from_iter([
             Tag::lang("aa"), 
             Tag::lang("aa").region("ET"), 
             Tag::lang("aa").script("Latn"),
             Tag::lang("aa").script("Latn").region("ET")])));
-        let aa_arab: Arc<TagSet> = Arc::new(TagSet(HashSet::from_iter([
+        expected.add_tagset(TagSet(HashSet::from_iter([
             Tag::lang("aa").script("Arab"), 
             Tag::lang("aa").script("Arab").region("ET")])));
-        let mut expected = LangTagsInner::new();
-        expected.extend(aa.iter().cloned().map(|t| (t, aa.clone())));
-        expected.extend(aa_arab.iter().cloned().map(|t| (t, aa_arab.clone())));
 
-        assert_eq!(test.0, expected);
+        assert_eq!(test, expected);
     }
 
     #[test]
@@ -133,12 +151,12 @@ mod tests {
         assert_eq!(
             test, 
             [
-                "aa-Arab-ET: aa-Arab = aa-Arab-ET",
-                "aa-Arab: aa-Arab = aa-Arab-ET",
-                "aa-ET: aa = aa-ET = aa-Latn = aa-Latn-ET",
-                "aa-Latn-ET: aa = aa-ET = aa-Latn = aa-Latn-ET",
-                "aa-Latn: aa = aa-ET = aa-Latn = aa-Latn-ET",
-                "aa: aa = aa-ET = aa-Latn = aa-Latn-ET",
+                "aa-Arab-ET: aa-Arab=aa-Arab-ET",
+                "aa-Arab: aa-Arab=aa-Arab-ET",
+                "aa-ET: aa=aa-ET=aa-Latn=aa-Latn-ET",
+                "aa-Latn-ET: aa=aa-ET=aa-Latn=aa-Latn-ET",
+                "aa-Latn: aa=aa-ET=aa-Latn=aa-Latn-ET",
+                "aa: aa=aa-ET=aa-Latn=aa-Latn-ET",
             ]);
     }
 }
